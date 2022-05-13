46ed712a98fbf5d67ac1516690d8d3be1ca297ca
Amirlan11 <a-zhakup@mail.ru>
2022-05-13 23:04:11 +0600

    dfgdf

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-//const port = 3000;
+const port = 3000;
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const https = require("https");
@@ -61,14 +61,6 @@ app.post('/booking',function (req ,res){
     newUser.save();
     res.redirect("/booking")
 })
-
-
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
-
 
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
