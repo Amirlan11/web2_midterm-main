a9b2c4362bb2f3ce231b5935d14aeb3ee398b030
Amirlan11 <a-zhakup@mail.ru>
2022-06-02 02:05:56 +0600

    CRUD

--- controllers/UserController.js
+++
@@ -1,9 +0,0 @@
-// const UserModel = require('../models/UserModel')
-//
-// const passport = require("passport");
-//
-// //level 6
-// exports.authGoogle = async (req, res) => {
-//     console.log("asd")
-//     passport.authenticate('google',{ scope: ["profile"] })
-// }
--- server.js
+++ server.js
@@ -1,20 +1,50 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const exphbs = require("express-handlebars");
 const ejs = require('ejs')
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override')
+const dbConfig = require('./config/database.config.js');
 const https = require("https");
 const app = express();
+
+const foodRoute = require('./routes/admin-routes');
+
+// Swagger
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const SwaggerOptions = require('./swagger/swagger.json');
+const swaggerDocument = swaggerJsDoc(SwaggerOptions);
+
+
+app.use(express.json())
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin",'*');
+    res.setHeader("Access-Control-Allow-Methods",'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Headers",'Content-Type,Authorization');
+    next();
+})
+
+app.use('/api', foodRoute);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+
+app.use((error,req,res,next)=>{
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message:message});
+})
+
+// authentification
 const session = require('express-session');
 const passport = require('passport');
 require('./auth');
+
 // Middleware to check if the user is authenticated
 function isLoggedIn(req, res, next) {
     req.user ? next() : res.sendStatus(401);
 }
-
-
 
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
@@ -51,23 +81,7 @@ app.get('/auth/google/failure', (req, res) => {
     res.send('Failed to authenticate..');
 });
 
-
-
-
-
-
-
-
-
-
-
 const PORT = 3000;
-
-
-const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
-})
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -75,33 +89,14 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 app.set('view engine', ejs);
 app.engine('ejs', require('ejs').__express);
-
 app.use(methodOverride('_method'))
-
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-
 app.use(express.static('public'))
 app.use(express.urlencoded({
     extended: true
 }))
 app.use(express.json())
 
-
-
-
-
-
-
-const dbConfig = require('./config/database.config.js');
-
-
-
-
-
-
-
-
+// routes
 
 app.set('view engine', 'ejs')
 
@@ -113,9 +108,7 @@ app.use('/booking', require('./routes/bookingRouter.js'))
 app.use('/admin', require('./routes/adminRoute'))
 app.use('/signup', require('./routes/signupRoute'))
 
-
-
-
+///database connection
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.get('/', (req, res) => {
@@ -130,20 +123,7 @@ mongoose.connect(dbConfig.url, {
     console.log('Could not connect to the database', err);
     process.exit();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//crud
 
 app.get('/', (req, res) => {
     res.render('/');
@@ -160,15 +140,6 @@ app.get('/update', (req, res) => {
 app.get('/delete', (req, res) => {
     res.render('delete');
 });
-
-
-
-
-
-
-
-
-
 
 let port = process.env.PORT;
 if (port == null || port == "") {
